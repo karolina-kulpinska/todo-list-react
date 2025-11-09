9df35dff133ba865e389477c66b5041383828d8a
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-11-09 17:46:43 +0100

    Adding numbering to the list

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -23,7 +23,7 @@ const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
 
                 <span
                     className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
-                    {task.content}
+                    {task.id} - {task.content}
                 </span>
 
                 <button
