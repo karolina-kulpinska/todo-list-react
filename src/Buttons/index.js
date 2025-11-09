6b41c5c36f6756a90821d8fee191ed44b5174152
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-11-09 20:35:15 +0100

    Code shortening

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,11 +1,10 @@
-import React from "react";
 import "./style.css";
 
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
 
     <div className="buttons">
         {tasks.length > 0 && (
-            <React.Fragment>
+            <>
                 <button
                     onClick={toggleHideDone}
                     className="buttons__button " >
@@ -18,13 +17,9 @@ const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
                 >
                     Ukończ wszystkie
                 </button>
-            </React.Fragment>
+            </>
         )}
-
-
-
     </div>
-
 );
 
 export default Buttons; 
