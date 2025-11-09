d431eb2b7f5eb36b30c0fd3ce9cfc12303cdc500
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-11-09 20:29:39 +0100

    class change

--- src/Section/index.js
+++ src/Section/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 const Section = ({ title, body, extraHeaderContent }) => (
     <section className="section">
         <header className="section__header">
-            <h2 className="section__header">{title}</h2>
+            <h2 className="section__title">{title}</h2>
             {extraHeaderContent}
         </header>
         <div className="section__body">
